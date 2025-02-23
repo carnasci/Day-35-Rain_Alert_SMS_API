adb6cf7650715ab718186ee4d119104068d5c8db
carnasci <cen.master24@gmail.com>
2025-02-23 17:29:33 -0600

    Day 35 - After looking at solution adjusted code to only notify once to bring an umbrella and I liked the for loop used to loop through response data instead of doing a range

--- Course Work/main.py
+++ Course Work/main.py
@@ -12,9 +12,17 @@ response = requests.get(url="https://api.openweathermap.org/data/2.5/forecast", 
 response.raise_for_status()
 print(response.status_code)
 data = response.json()
-id = data["list"][0]["weather"][0]["id"]
-for i in range(4):
-    forecast_id = data["list"][i]["weather"][0]["id"]
-    print(forecast_id)
-    if forecast_id < 700:
-        print("Bring an umbrella.")
+# id = data["list"][0]["weather"][0]["id"]
+# for i in range(4):
+#     forecast_id = data["list"][i]["weather"][0]["id"]
+#     print(forecast_id)
+#     if forecast_id < 700:
+#         print("Bring an umbrella.")
+#After solution
+will_rain = False
+for hourly_data in data["list"]:
+    condition_code = hourly_data["weather"][0]["id"]
+    if condition_code < 700:
+        will_rain = True
+if will_rain:
+    print("Bring an umbrella.")
