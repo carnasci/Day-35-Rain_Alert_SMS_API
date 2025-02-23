73b332071c5bc46cac2c1f10195b732103b09304
carnasci <cen.master24@gmail.com>
2025-02-23 17:20:30 -0600

    Day 35 - Second challenge complete, print weather condition code for the next 12 hours and alert to bring an umbrella

--- Course Work/main.py
+++ Course Work/main.py
@@ -1,14 +1,20 @@
 import requests
+from credentials import api_key
 
 parameters = {
     "lat": 38.6295,
     "lon": -90.1983,
     "units": "imperial",
-    "appid": "e662b0f9a40a2ea650f96201a99ad2a7",
-
+    "appid": api_key,
+    "cnt": 4,
 }
 response = requests.get(url="https://api.openweathermap.org/data/2.5/forecast", params=parameters)
 response.raise_for_status()
 print(response.status_code)
 data = response.json()
-print(data)
+id = data["list"][0]["weather"][0]["id"]
+for i in range(4):
+    forecast_id = data["list"][i]["weather"][0]["id"]
+    print(forecast_id)
+    if forecast_id < 700:
+        print("Bring an umbrella.")
